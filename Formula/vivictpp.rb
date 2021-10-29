dc9665c69e2f53ff2f76fe54e0580b30cf2062bf
Gustav Grusell <gustav.grusell@svt.se>
2021-10-29 15:53:10 +0200

    Use version 0.1.4
    Signed-off-by: Gustav Grusell <gustav.grusell@svt.se>

--- Formula/vivictpp.rb
+++ Formula/vivictpp.rb
@@ -1,10 +1,9 @@
 class Vivictpp < Formula
   desc "Vivict++ is an easy to use tool for subjective comparison of the visual quality of different encodings of the same video source."
   homepage ""
-  url "https://github.com/svt/vivictpp/archive/refs/tags/v0.1.3.tar.gz"
-  sha256 "8369ad06c3fc1e21d4a3ac7e4e43027b2d995780448f5df5c2a02e8e857b9ad8"
+  url "https://github.com/svt/vivictpp/archive/refs/tags/v0.1.4.tar.gz"
+  sha256 "cbee0ecdc5fc03eb985f616618cf6ae0e5d6450236924efcb893834fffaea8cc"
   head "https://github.com/svt/vivictpp.git", :branch => "main"
-#  option "with-ffmpeg-encore" "Use ffmpeg-encore brew formula instead of standard ffmpeg"
   license "GPL-2.0-or-later"
 
   depends_on "meson" => :build
