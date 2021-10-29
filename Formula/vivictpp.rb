8d5e564765d026463e8692fd68b1ae79bdd4a577
Gustav Grusell <gustav.grusell@svt.se>
2021-10-29 22:45:36 +0200

    Use dev branch for head
    Signed-off-by: Gustav Grusell <gustav.grusell@svt.se>

--- Formula/vivictpp.rb
+++ Formula/vivictpp.rb
@@ -3,7 +3,7 @@ class Vivictpp < Formula
   homepage ""
   url "https://github.com/svt/vivictpp/archive/refs/tags/v0.1.4.tar.gz"
   sha256 "cbee0ecdc5fc03eb985f616618cf6ae0e5d6450236924efcb893834fffaea8cc"
-  head "https://github.com/svt/vivictpp.git", :branch => "main"
+  head "https://github.com/svt/vivictpp.git", :branch => "dev"
   license "GPL-2.0-or-later"
 
   depends_on "meson" => :build
