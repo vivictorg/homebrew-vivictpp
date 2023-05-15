50cd6dc4deff1977c95f410f1628fcc0084a15d7
Gustav Grusell <gustav.grusell@gmail.com>
2023-05-15 21:40:50 +0200

    Update to v0.3.0
    Signed-off-by: Gustav Grusell <gustav.grusell@gmail.com>

--- Formula/vivictpp.rb
+++ Formula/vivictpp.rb
@@ -1,8 +1,8 @@
 class Vivictpp < Formula
   desc "Vivict++ is an easy to use tool for subjective comparison of the visual quality of different encodings of the same video source."
   homepage "https://github.com/vivictorg/vivictpp"
-  url "https://github.com/vivictorg/vivictpp/archive/refs/tags/v0.2.4.tar.gz"
-  sha256 "27c6a0dec6fc7854e38607a8333344c7680ca560ae16bae88f2685fd52df5d06"
+  url "https://github.com/vivictorg/vivictpp/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "f05d9da184f0017bffff14ac2999421c1cf24fac1e4f2ae9c95fdb5ea81eaea2"
   head "https://github.com/vivictorg/vivictpp.git", :branch => "dev"
   license "GPL-2.0-or-later"
 
